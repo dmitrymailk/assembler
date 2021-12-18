5f987fa81b927acc056f85db002534151c45fd94
dim web <dmitrymail_k@mail.ru>
2021-12-18 19:04:51 +0300

    access to variables from c code

--- connect_mylab_and_c/test.c
+++ connect_mylab_and_c/test.c
@@ -2,10 +2,16 @@
 #include <inttypes.h>
 
 void calc_two_numbers_expression();
-void unit_test();
+// void unit_test();
+extern int64_t number_1;
+extern int64_t number_2;
 int main()
 {
-  unit_test();
+  int64_t *num_1 = &number_1;
+  int64_t *num_2 = &number_2;
+  *num_1 = (int64_t)2;
+  *num_2 = (int64_t)40;
+  // unit_test();
   calc_two_numbers_expression();
   return 0;
 }
